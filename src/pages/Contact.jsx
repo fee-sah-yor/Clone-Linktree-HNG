e7e03603cc75bed5978602e757bd446a57941e33
fee-sah-yor <101174144+fee-sah-yor@users.noreply.github.com>
2022-11-05 23:29:10 +0100

    second update on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,44 +4,49 @@ import Footer from '../components/Footer';
 
 const Contact = () => {
     return (
-        <div className='container'>
-           <div className='sub_container'>
-           <h1>Contact Me</h1>
-           <h5>Hi there, contact me to ask me about anything you have in mind.</h5>
+        <>
+            <div className='container'>
+                <div className='sub_container'>
+                    <h1>Contact Me</h1>
+                    <h5>Hi there, contact me to ask me about anything you have in mind.</h5>
 
-           <form action="contact" className='contactform'>
-               <div class="input-box">
-                   <label htmlFor="first_name" className='values'>Firstname</label>
-                   <input type="text" placeholder='Enter your first name' id='first_name' />
-               </div>
+                    <form action="contact" className='contactform'>
+                        <div className='identity'>
+                            <div class="input-box">
+                                <label htmlFor="first_name" className='values'>Firstname</label>
+                                <input type="text" placeholder='Enter your first name' id='first_name' />
+                            </div>
 
-               <div class="input-box">
-                   <label htmlFor="last_name" className='values'>Lastname</label>
-                   <input type="text" placeholder='Enter your last name' id='last_name' />
-               </div>
+                            <div class="input-box">
+                                <label htmlFor="last_name" className='values'>Lastname</label>
+                                <input type="text" placeholder='Enter your last name' id='last_name' />
+                            </div>
+                        </div>
 
-               <div class="input-box">
-                   <label htmlFor="email" className='values'>Email</label>
-                   <input type="email" placeholder='youremail@email.com' id='email' />
-               </div>
+                        <div class="input-box">
+                            <label htmlFor="email" className='values'>Email</label>
+                            <input type="email" placeholder='youremail@email.com' id='email' />
+                        </div>
 
-               <div class="input-box">
-               <label htmlFor="email" className='values'>Message</label>
-                   <textarea type="text" placeholder="Send me a message and I'll reply you as soon as possible..."
-                       id='message'>
-                   </textarea>
-               </div>
+                        <div class="input-box">
+                            <label htmlFor="email" className='values'>Message</label>
+                            <textarea type="text" placeholder="Send me a message and I'll reply you as soon as possible..."
+                                id='message'>
+                            </textarea>
+                        </div>
 
-               <label for="t and c">You agree to providing your data to Fisayo who may contact you</label>
-               <input type="checkbox"name="t and c" className='checkbox' value="t and c"/>
+                        <div className='checkbox' >
+                            <label for="t and c">You agree to providing your data to Fisayo who may contact you</label>
+                            <input type="checkbox" name="t and c" value="t and c" />
+                        </div>
+                        <button type="submit" className="submit">Send message</button>
 
-               <button type="submit" className="submit">Send message</button>
-
-                   </form>
-                   </div>
-                   <Footer/>
-                   </div>
-                )
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
 }
 
-                export default Contact;
+export default Contact;
